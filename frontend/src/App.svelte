a3c3c634a8fc29612abb4e6f9a96b6de87c7577f
MateMate <66790113+tanjirogta@users.noreply.github.com>
2025-05-26 13:57:52 +0200

    implemented sveltestrap and started navbar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,15 @@
 <script>
-  
+    import { onMount } from 'svelte';
+    import Header from './lib/header.svelte';
 </script>
+
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</svelte:head>
 
 <main>
   
-  
+    <Header />
 
 </main>
 
---
+++ frontend/src/lib/header.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import {
+        Styles,
+        Collapse,
+        NavbarToggler,
+        NavbarBrand,
+        Nav,
+        Navbar,
+        NavItem,
+        NavLink,
+        Dropdown,
+        DropdownToggle,
+        DropdownMenu,
+        DropdownItem,
+    } from "@sveltestrap/sveltestrap";
+
+    let isOpen = false;
+
+    function handleUpdate(event) {
+        isOpen = event.detail.isOpen;
+    }
+</script>
+
+<Navbar color="dark" dark expand="md" container={false} class="fixed-top w-100">
+    <NavbarBrand href="/">
+        <img src="../assets/icon_512x512.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top me-2" />
+        Crying Obsidian
+    </NavbarBrand>
+    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ms-auto" navbar>
+            <NavItem>
+                <NavLink href="/">Lorem</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="/"
+                    >Ipsum</NavLink
+                >
+            </NavItem>
+            <Dropdown nav inNavbar>
+                <DropdownToggle nav caret>Options</DropdownToggle>
+                <DropdownMenu end>
+                    <DropdownItem>Option 1</DropdownItem>
+                    <DropdownItem>Option 2</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+        </Nav>
+    </Collapse>
+</Navbar>
